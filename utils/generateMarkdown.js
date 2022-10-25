f066c8db2e1e6a7f60d9d5c1df4c4aded8e2aee1
Andy L <andresslemus3@gmail.com>
2022-10-25 01:28:55 -0700

    added code to display username and github on questions section

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,5 +1,3 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
 function renderLicenseBadge(license) {
   if (license === 'No License') {
     return '';
@@ -8,8 +6,6 @@ function renderLicenseBadge(license) {
   }
 }
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
 function renderLicenseLink(license) {
   if (license === 'MIT') {
     return `https://lbesson.mit-license.org/`;
@@ -28,9 +24,6 @@ function renderLicenseLink(license) {
   }
 }
 
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
 function renderLicenseSection(license) {
   if (license === 'No License') {
     return '';
@@ -40,7 +33,6 @@ function renderLicenseSection(license) {
   }
 }
 
-// TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
 
@@ -52,6 +44,7 @@ function generateMarkdown(data) {
   * [Usage](#usage)
   * [Contributors](#contributors)
   * [Licenses](#licenses)
+  * [Questions](*questions)
   
   ## Description
   ${data.description}
@@ -66,6 +59,11 @@ function generateMarkdown(data) {
   Contributors: ${data.contributions}
 
   ${renderLicenseSection(data.licenses)}
+
+  ## Questions
+  Do you have questions about this project?  
+  GitHub: https://github.com/${data.githubName}  
+  Email: ${data.email}
 `;
 }
 
